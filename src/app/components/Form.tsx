e135a796c51fa27168ac3f0373ccd90b0c01b491
DucViet1021998 <viethg1021998@gmail.com>
2023-05-31 03:56:27 +0700

    firstcommit

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,183 @@
+'use client';
+
+import React from 'react';
+import { Box, Button, TextField, InputAdornment, Grid } from '@mui/material';
+// import {PersonIcon,  LockOpenIcon } from '@mui/icons-material';
+import { Person, LockOpen, VpnKey, Mail, Phone } from '@mui/icons-material';
+import { z, ZodType } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+type FormData = {
+    userName: string;
+    phone: string | number;
+    email: string;
+    password: string;
+    confirmPassword: string;
+};
+
+export default function Form() {
+    const schema: ZodType<FormData> = z
+        .object({
+            userName: z
+                .string()
+                .nonempty({
+                    message: 'User Name is required!',
+                })
+                .min(5)
+                .regex(new RegExp('^[a-zA-Z0-9.]*$'), 'Not contain special characters'),
+            phone: z
+                .string()
+                .trim()
+                .min(10, { message: 'Phone numbers are a minium of 10 digits' })
+                .length(10, { message: 'Ten numbers are required!' })
+                .regex(/(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/, {
+                    message: 'Only numbers are allowed',
+                }),
+            email: z
+                .string()
+                .nonempty({
+                    message: 'Email is required!',
+                })
+                .email({ message: 'Invalid email address' }),
+            password: z
+                .string()
+                .nonempty({
+                    message: 'Password is required!',
+                })
+                .min(8),
+            confirmPassword: z.string().min(8).nonempty({
+                message: 'Please confirm your password!',
+            }),
+        })
+        .refine((data) => data.password === data.confirmPassword, {
+            message: 'Password not match!',
+            path: ['confirmPassword'],
+        });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+    const submitData = (data: FormData) => {
+        alert('Bật F12 để xem value trong console anh nhé!');
+        console.log(data);
+    };
+
+    return (
+        <Box component="form" noValidate autoComplete="off" onSubmit={handleSubmit(submitData)}>
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                // spacing={2}
+                alignItems="center"
+            >
+                <Grid xs={12} item>
+                    <TextField
+                        label="Username"
+                        fullWidth
+                        required
+                        margin="normal"
+                        {...register('userName')}
+                        helperText={errors['userName'] ? errors['userName'].message : ''}
+                        error={!!errors['userName']}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Person />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Grid>
+                <Grid xs={12} item>
+                    <TextField
+                        label="Email"
+                        fullWidth
+                        margin="normal"
+                        required
+                        type="email"
+                        {...register('email')}
+                        error={!!errors['email']}
+                        helperText={errors['email'] ? errors['email'].message : ''}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Mail />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Grid>
+
+                <Grid xs={12} item>
+                    <TextField
+                        label="Phone"
+                        fullWidth
+                        margin="normal"
+                        {...register('phone')}
+                        error={!!errors['phone']}
+                        helperText={errors['phone'] ? errors['phone'].message : ''}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Phone />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Grid>
+
+                <Grid xs={12} item>
+                    <TextField
+                        label="Password"
+                        fullWidth
+                        required
+                        margin="normal"
+                        type="password"
+                        error={!!errors['password']}
+                        {...register('password')}
+                        helperText={errors['password'] ? errors['password'].message : ''}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockOpen />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Grid>
+
+                <Grid xs={12} item>
+                    <TextField
+                        label="Confirm Password"
+                        fullWidth
+                        required
+                        margin="normal"
+                        type="password"
+                        {...register('confirmPassword')}
+                        error={!!errors['confirmPassword']}
+                        helperText={
+                            errors['confirmPassword'] ? errors['confirmPassword'].message : ''
+                        }
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <VpnKey />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Grid>
+                <Grid xs={12} item>
+                    <Button size="large" type="submit" variant="contained">
+                        Submit
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import Image from 'next/image';
+import Form from './components/Form';
+import { Grid } from '@mui/material';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <Grid
+                style={{ height: '100vh' }}
+                container
+                direction="column"
+                // spacing={2}
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Grid item>
+                    <img width="218" height="78" src="https://i.imgur.com/WbxNsdl.png" alt="logo" />
+                </Grid>
+                <Grid
+                    style={{
+                        backgroundColor: 'rgba(255,255,255, 0.4)',
+                        borderRadius: '10px',
+                        padding: '10px 100px 50px 100px',
+                    }}
+                    item
+                    // xs={6}
+                >
+                    <Form />
+                </Grid>
+            </Grid>
+        </main>
+    );
 }
